5a3b8b9bb9713785033a60d771fb1a2ce58c72d4
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-09-12 23:41:18 +0100

    built gui

--- get_ffMPEG.py
+++ get_ffMPEG.py
@@ -1,11 +1,17 @@
 import subprocess
 import os
+import subprocess
+import os
+import shutil
+
 # Specify the URL of the gyan.dev repository
 ffmpeg_url = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
 
 # Specify the directory where you want to install FFmpeg
 install_dir = "C:\Remove\\"
 
+# create the directory if it doesn't exist
+os.makedirs(install_dir, exist_ok=True)
 
 # Define the command to download and extract the FFmpeg binaries
 download_command = [
@@ -27,7 +33,23 @@ extract_command = [
 try:
     subprocess.run(download_command, check=True)
     subprocess.run(extract_command, check=True)
-    os.remove("ffmpeg.zip")  # Remove the downloaded ZIP file
-    print("FFmpeg installed successfully!")
+
+    # Find the subdirectory in the installation directory
+    subdirs = [d for d in os.listdir(install_dir) if os.path.isdir(os.path.join(install_dir, d))]
+
+    if len(subdirs) == 1:
+        subdir_name = subdirs[0]
+        
+        # Move all files and subdirectories from the subdirectory to the main directory
+        subdir_path = os.path.join(install_dir, subdir_name)
+        for item in os.listdir(subdir_path):
+            item_path = os.path.join(subdir_path, item)
+            if os.path.isfile(item_path) or os.path.isdir(item_path):
+                shutil.move(item_path, install_dir)
+
+        # Remove the now-empty subdirectory
+        os.rmdir(subdir_path)
+
+        
 except subprocess.CalledProcessError as e:
     print(f"Error: {e}")
